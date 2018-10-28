adc31887188b1e2e300588ccd05b7b0fb27e3ca5
Thiago Loureiro <thiagoguaru@me.com>
2018-10-28 22:01:14 +0100

    Initial Commit

---
+++ content/OrleansGrainInterface/IHello.cs
@@ -0,0 +1,10 @@
+﻿using Orleans;
+using System.Threading.Tasks;
+
+namespace OrleansGrainInterface
+{
+    public interface IHello : IGrainWithIntegerKey
+    {
+        Task<string> SayHello(string greeting);
+    }
+}
